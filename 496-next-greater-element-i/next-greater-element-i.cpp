10f6b877f996665a690f051f0d08381381730f64
Palak Lohade <137188866+palaklohade@users.noreply.github.com>
2025-08-29 16:30:23 +0530

    Time: 3 ms (41.93%) | Memory: 12.7 MB (61.05%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        int n = nums1.size();
-        vector<int>result( n, -1);
-        unordered_map<int , int> freq;
+        unordered_map<int , int> map;
         stack<int> st;
 
-        for(int num = 0 ; num < n ; num++){
-            freq[nums1[num]] = num;
-        }
-
         for(int num : nums2){
-            while(!st.empty() && num > st.top() ){
-                int top = st.top(); st.pop();
-
-                if(freq.count(top)){
-                    result[freq[top]] = num;
-                }
-
+            while(!st.empty() && num > st.top()){
+                map[st.top()] = num;
+                st.pop();
             }
             st.push(num);
+        }
+
+        while(!st.empty()){
+        map[st.top()] = -1;
+        st.pop();
+        }
+
+        vector<int> result;
+        for(int num : nums1){
+            result.push_back(map[num]);
         }
         return result;
     }
