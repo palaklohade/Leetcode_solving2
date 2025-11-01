ea700cb2b077880c34d26f1ab8bb19596d9f2191
Palak Lohade <137188866+palaklohade@users.noreply.github.com>
2025-11-01 21:47:12 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (17.31%) - LeetSync

---
+++ 172-factorial-trailing-zeroes/factorial-trailing-zeroes.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int trailingZeroes(int n) {
+        int ans = 0;
+        int i = 5;
+
+        while( n >= i){
+            ans += (n / i);
+            i *= 5;
+        }
+        return ans;
+    }
+};
