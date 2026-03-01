173f6620a579b53413089c3b6744c9ebf27b3939
Palak Lohade <137188866+palaklohade@users.noreply.github.com>
2026-03-01 12:54:07 +0530

    Time: 3 ms (27.63%) | Memory: 10.5 MB (87.82%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
 
-    void backtrack(int start, int n , vector<int>& nums ,vector<vector<int>>& result ){
+    void backtrack(int start , int n , vector<int>&nums , vector<vector<int>>&result){
         if(start == n){
             result.push_back(nums);
             return;
         }
-for (int i = start; i < nums.size(); ++i) {
-            swap(nums[start], nums[i]);
-            backtrack(start + 1, n , nums, result);
-            swap(nums[start], nums[i]); 
-            }
+        for(int i =start ; i < n ; i++){
+           swap(nums[start] , nums[i]);
+           backtrack(start + 1 , n , nums,result);
+           swap(nums[start] , nums[i]);
+
+        }
     }
-    
     vector<vector<int>> permute(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>> result ;
-        backtrack(0, n , nums,result);
+        vector<vector<int>> result;
+        backtrack(0,n,nums, result);
         return result;
-
     }
 };
